143ba6b02d4ad696d5dfd4fa509539c4a7141d68
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-07-06 22:42:17 +0100

    structure added

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Description from './Description/Description';
+// import Option from "./Option/Option";
+// import Feedbeck from "./Feedback/Feedbeck";
+
+const App = () => {
+  return (
+      <>
+          <Description />
+          {/* <Option /> */}
+          {/* <Feedback /> */}
+      </>
+  )
+}
+
+export default App;
