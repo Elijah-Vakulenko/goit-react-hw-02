cc7c34bacc46dff976bd8f4b2b156dd3d3fb266f
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-07-07 22:17:52 +0100

    it works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Description from './Description/Description';
 import Option from "./Option/Option";
-// import Feedbeck from "./Feedback/Feedbeck";
+import Feedback from "./Feedback/Feedback";
+import Notification from "./Notification/Notification";
 
-import s from './App.module.css'
+import s from './App.module.css';
 
 const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
       <div className={s.container}>
           <Description />
-          <Option />
-          {/* <Feedback /> */}
+          <Option updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
+          {totalFeedback > 0 ? <Feedback feedback={feedback} /> : <Notification message="No feedback given" />}
       </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import s from './Option.module.css';
 
-import s from './Option.module.css'
-
-const Option = () => {
+const Option = ({ updateFeedback, resetFeedback, totalFeedback }) => {
   return (
       <div className={s.btnContainer}>
-          <button className={s.optionBtn} type="button">Good</button>
-          <button className={s.optionBtn} type="button">Neutral</button>
-          <button className={s.optionBtn} type="button">Bad</button>
-          <button className={s.optionBtn} type="button">Reset</button>
-    </div>
-  )
+          <button onClick={() => updateFeedback("good")} className={s.optionBtn} type="button">Good</button>
+          <button onClick={() => updateFeedback("neutral")} className={s.optionBtn} type="button">Neutral</button>
+          <button onClick={() => updateFeedback("bad")} className={s.optionBtn} type="button">Bad</button>
+          {totalFeedback > 0 && (
+              <button onClick={resetFeedback} className={s.optionBtn} type="button">Reset</button>
+          )}
+      </div>
+  );
 }
 
 export default Option;
